6fa338fdb1c72dea1c4c1eb91750adac928ef457
Varun Kodamanchili <varun132413@gmail.com>
2024-07-07 09:18:12 +0530

    implemented API in code

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request,flash,url_for
+import os
+from dotenv import load_dotenv
+from script import get_data
+app = Flask(__name__)
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
+os.makedirs('static/images', exist_ok=True)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        img = request.files['file']
+        img.save(os.path.join('static/images',img.filename))
+        data=get_data(img) #method to script
+        flash(f"File {img.filename} uploaded successfully")
+        image_url = url_for('static', filename=f'images/{img.filename}')
+        return render_template('index.html',image_url=image_url,data=data)
+    return render_template('index.html',image_url="https://res.cloudinary.com/durc5ydxo/image/upload/v1720244864/upload-icon_kb99jl.png")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ script.py
@@ -0,0 +1,24 @@
+import os
+import PIL.Image
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv('API_key'))
+
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(model_name="gemini-1.5-flash",generation_config=generation_config)
+
+def get_data(img):
+    img = PIL.Image.open(f"static/images/{img.filename}")
+    response = model.generate_content(["General description about dog breed in the image, including history and origin",img],stream=True)
+    response.resolve()
+    return response.text
